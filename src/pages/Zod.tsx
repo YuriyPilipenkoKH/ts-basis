c569bc8c4b964c2a7c1ea4303c2d05e1115760d0
Yuriy Pilipenko <yurik2061@gmail.com>
2023-11-18 17:55:52 +0200

    implemented zod

--- src/pages/Zod.tsx
+++ src/pages/Zod.tsx
@@ -1,10 +1,32 @@
+import { z } from "zod"
 import { Container } from "../components/Container/Container"
 
 
+
+const UserSchema = z.object({
+    name: z.string().min(2).max(32),
+    age: z.number().gt(0),
+    dob: z.date().optional(),
+    isProgrammer: z.boolean().default(true).optional(),
+    hobby: z.enum(['coding', 'driving'])
+})
+type User = z.infer<typeof UserSchema>
+
+const user : User = { 
+    name: 'Bant',
+    age: 20,
+    dob: new Date(),
+    isProgrammer: true,
+    hobby: 'coding'
+}
+
+console.log(UserSchema.safeParse(user).success)
+ 
 const Zod = () => {
   return (
     <Container>
       zodd
+   
     </Container>
   )
 }
