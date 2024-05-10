d6023c24db46dad28c9baf196fbaebc4c7ebcfad
Yuriy Pilipenko <yurik2061@gmail.com>
2024-05-10 21:36:00 +0300

    useLocation

--- src/pages/ReactQuery.tsx
+++ src/pages/ReactQuery.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet, useLocation } from 'react-router-dom'
 
 function ReactQuery() {
+	const location = useLocation();
+
   return (
-    <div>
-      <div>
-        ReactQuery.tsx
-      </div>
-      <Link to="rqSupreheroes">RQ Super Heroes</Link>
+    <>
+		{location.pathname === '/reactQuery' && (
+      <>
+				<div>
+					ReactQuery.tsx
+				</div>
+				<Link to="rqSupreheroes">RQ Super Heroes</Link>
+			</>
+
+		)}
+			{location.pathname === '/reactQuery/rqSupreheroes' && (
+
 			<Outlet />
-    </div>
+			)}
+    </>
   )
 }
 
